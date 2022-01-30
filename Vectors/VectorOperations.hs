052a478d8a52ba2ad72aea98baf71c4855b4eee5
icen1 <kokeamr@hotmail.com>
2022-01-30 17:14:09 +0000

    Added vectorDistance to get the distance between two vectors

--- Vectors/VectorOperations.hs
+++ Vectors/VectorOperations.hs
@@ -9,10 +9,10 @@ data Vector
 
 -- An operation to handle the addition of vectors to each other. Tried using Maybe/Nothing/Just instead of error but couldn't. Research later.
 vectorAddition :: Vector -> Vector -> Vector
-vectorAddition (TwoD x  y) (TwoD a b) = TwoD (x+a) (y+b)
-vectorAddition (ThreeD x  y  z) (ThreeD a b c) = ThreeD (x+a) (y+b) (z+c)
-vectorAddition (ThreeD x  y  z) (TwoD a b) = error "Can't be different dimensions"
-vectorAddition (TwoD a b) (ThreeD x  y  z) = error "Can't be different dimensions"
+vectorAddition (TwoD x y) (TwoD a b) = TwoD (x+a) (y+b)
+vectorAddition (ThreeD x y z) (ThreeD a b c) = ThreeD (x+a) (y+b) (z+c)
+vectorAddition (ThreeD x y z) (TwoD a b) = error "Can't be different dimensions"
+vectorAddition (TwoD a b) (ThreeD x y z) = error "Can't be different dimensions"
 
 -- An operation to handle the multiplication of vectors to scalar constants
 scalarMultiplication :: Float -> Vector -> Vector
@@ -23,3 +23,11 @@ scalarMultiplication a (ThreeD x  y  z) = ThreeD (x*a) (y*a) (z*a)
 vectorLength :: Vector -> Float
 vectorLength (TwoD x y) =  sqrt((x^2)+(y^2))
 vectorLength (ThreeD x y z) = sqrt(x^2+y^2+z^2)  
+
+-- An operation to get the distance between two vectors
+vectorDistance :: Vector -> Vector -> Float 
+vectorDistance (TwoD x y) (TwoD a b) = sqrt((x-a)^2+(y-b)^2)
+vectorDistance (ThreeD x y z) (ThreeD a b c) = sqrt((x-a)^2+(y-b)^2+(z-c)^2)
+vectorDistance (TwoD x y) (ThreeD a b c) =  error "Can't be different dimensions"
+vectorDistance (ThreeD x y z) (TwoD a b) = error "Can't be different dimensions"
+
